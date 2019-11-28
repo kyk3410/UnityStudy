6f804128454ad2f18c91b34fda206f655cf15b6d
YANGKYUNOTEBOOK\Yang-Kyu <kyk8229@naver.com>
2019-11-28 16:21:32 +0900

    6-1커밋

--- Sebastian Lague/Assets/Script/Enemy.cs
+++ Sebastian Lague/Assets/Script/Enemy.cs
@@ -7,18 +7,61 @@ using UnityEngine.AI;
 [RequireComponent (typeof (NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State {Idle, Chasing, Attacking};
+    State currentState;
+
     NavMeshAgent pathfinder;
     Transform target;
+
+    float attackDistanceThreshold = 1.5f;
+    float timeBetweenAttacks = 1f;
+
+    float nextAttackTime;
     protected override void Start()
     {
         base.Start ();
         pathfinder = GetComponent<NavMeshAgent>();
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
         StartCoroutine(UpdatePath());
     }
 
     void Update()
     {
+        if(Time.time > nextAttackTime)
+        {
+            float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+
+            if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold, 2))
+            {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+        Vector3 originalPosition = transform.position;
+        Vector3 attackPosition = target.position;
+
+        float attackSpeed = 3; 
+        float percent = 0;
+
+        while(percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath()
@@ -26,13 +69,15 @@ public class Enemy : LivingEntity
         float refreshRate = 0.25f;
         while (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-            if (!dead)
+            if(currentState == State.Chasing)
             {
-                pathfinder.SetDestination(target.position);
+                Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(target.position);
+                }
             }
             yield return new WaitForSeconds(refreshRate);
-            
         }
-    }
+    } 
 }
