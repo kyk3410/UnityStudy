fd1cb18bf28ecda969fcac142176f57733251440
YANGKYUNOTEBOOK\Yang-Kyu <kyk8229@naver.com>
2019-12-05 16:20:03 +0900

    12-2 오류 해결 13-1

--- Sebastian Lague/Assets/Editor/MapEditor.cs
+++ Sebastian Lague/Assets/Editor/MapEditor.cs
@@ -8,11 +8,17 @@ public class MapEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
-
         MapGenerator map = target as MapGenerator;
+        //  base.OnInspectorGUI();
+        if (DrawDefaultInspector())
+        {
+            map.GenerateMap();
+        }
 
-        map.GenerateMap();
+        if(GUILayout.Button("Generate Map"))
+        {
+            map.GenerateMap();
+        }
     }
     
 }
