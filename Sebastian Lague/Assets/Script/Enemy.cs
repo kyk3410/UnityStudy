9d14d13a7161d9d497bf75fdd2715338d0214492
YANGKYUNOTEBOOK\Yang-Kyu <kyk8229@naver.com>
2019-12-09 16:05:20 +0900

    15 커밋

--- Sebastian Lague/Assets/Script/Enemy.cs
+++ Sebastian Lague/Assets/Script/Enemy.cs
@@ -10,6 +10,8 @@ public class Enemy : LivingEntity
     public enum State {Idle, Chasing, Attacking};
     State currentState;
 
+    public ParticleSystem deathEffect;
+
     NavMeshAgent pathfinder;
     Transform target;
     Material skinMaterial;
@@ -47,6 +49,15 @@ public class Enemy : LivingEntity
             targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
             StartCoroutine(UpdatePath());
         }
+    }
+
+    public override void TakeHit(float damage, Vector3 hitPoint, Vector3 hitDirection)
+    {
+        if(damage >= health)
+        {
+            Destroy(Instantiate(deathEffect.gameObject, hitPoint, Quaternion.FromToRotation(Vector3.forward, hitDirection)) as GameObject, deathEffect.startLifetime);
+        }
+        base.TakeHit(damage, hitPoint, hitDirection);
     }
 
     void OnTargetDeath()
