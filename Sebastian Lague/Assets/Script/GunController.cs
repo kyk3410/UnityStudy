6021984643de093ac19925133a1c2296786dfcb0
YANGKYUNOTEBOOK\Yang-Kyu <kyk8229@naver.com>
2019-12-11 23:04:58 +0900

    1.해설 커밋

--- Sebastian Lague/Assets/Script/GunController.cs
+++ Sebastian Lague/Assets/Script/GunController.cs
@@ -18,11 +18,17 @@ public class GunController : MonoBehaviour
 
     public void EquipGun(Gun gunToEquip)
     {
+        // 새로운 총을 가져오고 싶을때, equipedGun에 총의 새로운 인스턴스를 생성해 넣을 수 있다.
+        // 하여, Instantiate(gunToEquip)을 할당한다.
         if(equippedGun != null)
         {
+            // 현재 장착중인 총이 이미 있는지 체크해야 되므로 이미 있으면, 새 총을 가져오기 전에 파괴해야 한다.
+            // 하여 equippedGun이 null이 아니면 Destroy(equippedGun.gameObject)를 해준다.
             Destroy(equippedGun.gameObject);
         }
         equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        // 이제 총기를 새로 인스턴스화 할때 위치 값으로 weaponHold.position와 weaponHold.rotation을 줄 수 있다.
+        // 총 오브젝트가 플레이어를 따라 움직이도록 weaponHold의 자식으로 넣어야 한다.
         equippedGun.transform.parent = weaponHold;
     }
 
