b881890c934fa891c321472393234a610e817bc8
YANGKYUNOTEBOOK\Yang-Kyu <kyk8229@naver.com>
2019-12-22 23:32:23 +0900

    Procedural 1-1커밋

---
+++ Procedural Cave Generation/Assets/MapGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0,100)]
+    public int randomFillPercent;
+
+    int[,] map;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random psuedoRandom = new System.Random(seed.GetHashCode());
+
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                map[x, y] = (psuedoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+            }
+        }
+    }
+
+}
