5f1374d3ed747322ffc96c57cfde9c25459ad4d6
YangKyuDeskTop <kyk8229@naver.com>
2020-01-05 23:23:18 +0900

    2 커밋

--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour
 {
-    Controller2D controller;
+    float moveSpeed = 6;
+    float gravity = -20;
+    Vector3 velocity;
 
+    Controller2D controller;
     void Start()
     {
         controller = GetComponent<Controller2D>();
     }
 
- 
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
