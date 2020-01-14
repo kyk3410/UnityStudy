12cebd3fbd161ba8b7c3af7d8e178a8c64ef6b6b
YangKyuDeskTop <kyk8229@naver.com>
2020-01-14 18:50:25 +0900

    Kinematic Equations 2,3 커밋

---
+++ Kinematic Equations/Assets/Motor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+    public float initialVelocity;
+    public float acceleration;
+    float currentVelocity;
+
+    void Start()
+    {
+        currentVelocity = initialVelocity;
+    }
+
+    void FixedUpdate()
+    {
+        if(Time.fixedTime < Timer.predictedTime)
+        {
+            currentVelocity += acceleration * Time.fixedDeltaTime;
+            transform.Translate(Vector3.right * currentVelocity * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Kinematic Equations/Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public static float predictedTime;
+    public Motor objectA;
+    public Motor objectB;
+
+    public float timeStep = 0.02f;
+    void Start()
+    {
+        Time.fixedDeltaTime = timeStep;
+
+        float h = objectA.transform.position.x - objectB.transform.position.x; 
+
+        float a = objectB.acceleration - objectA.acceleration;
+        float b = 2 * (objectB.initialVelocity - objectA.initialVelocity);
+        float c = -2 * h;
+
+        predictedTime = (-b + Mathf.Sqrt(b * b - 4 * a * c)) / (2 * a);
+        print(predictedTime); 
+    }
+
+}
