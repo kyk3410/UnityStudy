d77e321ad5df1da0c0eaaff7d6bf9de3c4ccc833
YangKyuDeskTop <kyk8229@naver.com>
2020-01-29 23:29:04 +0900

    4 해설

--- Procedural Landmass Generation/Assets/Scripts/MapGenerator.cs
+++ Procedural Landmass Generation/Assets/Scripts/MapGenerator.cs
@@ -13,6 +13,7 @@ public class MapGenerator : MonoBehaviour
     public float noiseScale;
 
     public int octaves;
+    // 59. 범위를 0~1사이로 지정해준다.
     [Range(0,1)]
     public float persistance;
     public float lacunarity;
@@ -31,6 +32,8 @@ public class MapGenerator : MonoBehaviour
     public void GenerateMap()
     {
         // 14. Noise 클래스의 2D 노이즈 맵을 검색한다
+        // 49. Noise클래스에서 추가해준 octaves,persistance,lacunarity를 추가해준다.
+        // 54. seed와 offset을 추가해준다.
         float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity,offset);
         // 15. 이 함수의 뒷부분에는 noiseMap을 처리하고 지형으로 바꾸는 모든 방법이 있다. 먼저 MapDisplay 클래스에만 전달 하고 있다.
         // 화면에 노이즈 맵 그리기를 시작할 수 있다.
@@ -64,7 +67,7 @@ public class MapGenerator : MonoBehaviour
             display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultipler, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         }
     }
-
+    // 58. 이 함수는 스크립트가로드되거나 Inspector에서 값이 변경 될 때 호출됩니다, 이하로 줄지않게 된다.
     void OnValidate()
     {
         if(lacunarity < 1)
--- Procedural Landmass Generation/Assets/Scripts/Noise.cs
+++ Procedural Landmass Generation/Assets/Scripts/Noise.cs
@@ -7,19 +7,22 @@ using UnityEngine;
 
 public static  class Noise
 {
-// 1. 노이즈 맵을 생성하는 기능을 갖기를 원하기때문에 우리는 그 함수가 0과 1 사이의 숫자 그리드를 반환하기를 원한다
-// 2. 함수는 lacunarity및 persistence를 포함하여 많은 논증을? 얻는다
-// 7. NoiseMap에 scale인수를 추가해준다.
-// 39. octaves와 persistance,lacunarity를 추가해준다
+    // 1. 노이즈 맵을 생성하는 기능을 갖기를 원하기때문에 우리는 그 함수가 0과 1 사이의 숫자 그리드를 반환하기를 원한다
+    // 2. 함수는 lacunarity및 persistence를 포함하여 많은 논증을? 얻는다
+    // 7. NoiseMap에 scale인수를 추가해준다.
+    // 39. octaves와 persistance,lacunarity를 추가해준다
+    // 50. seed도 새로 만들어준다 seed가 있으면 같은시드를 사용하면 같은지도를 얻을수있다.
     public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight,int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         // 3. mapWidth와 mapHeight가 새로운 2차원 float 배열로 정의한다.
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
+        // 51. 무작위로 시드를 전달할수가 있다.
         System.Random prng = new System.Random(seed);
         Vector2[] octaveOffsets = new Vector2[octaves];
         for(int i = 0; i<octaves; i++)
         {
+            // 53. offset.x,y를 더해준다
             float offsetX = prng.Next(-100000, 100000) + offset.x;
             float offsetY = prng.Next(-100000, 100000) + offset.y;
             octaveOffsets[i] = new Vector2(offsetX, offsetY);
@@ -31,10 +34,13 @@ public static  class Noise
             scale = 0.0001f;
         }
 
+        // 48. 최대 최소 소음높이일 설정해준다.
         float maxNoiseHeight = float.MinValue;
         float minNoiseHeight = float.MaxValue;
 
+        // 55. 너비는 지도너비를 2나눈값
         float halfWidth = mapWidth / 2f;
+        // 56. 높이는 지도높이를 2나눈값
         float halfHeight = mapHeight / 2f;
         // 4. noiseMap을 반복한다
         for(int y = 0; y < mapHeight; y++)
@@ -55,6 +61,8 @@ public static  class Noise
                 // 8. scale을 나누어준다, 정수가 아니기 때문에 깔끔하게 된다.
                 // 9. scale이 0과 같을 수 있으므로 0으로 나누기 때문에 주의해야된다.
                 // 43. 주파수를 적용해주고 샘플 좌표를 곱해준다.
+                // 52. octaveOffests[]을 추가할 수 있다.
+                // 57. x 값을 사용하는 대신 x-halfwidth하고 y값도 y-halfHeight한다
                     float sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x; 
                     float sampleY = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
                 // 11. Mathf.PerlinNoise(sampleX,sampleY)를 해줄수있다        
@@ -87,10 +95,13 @@ public static  class Noise
         {
             for (int x = 0; x < mapWidth; x++)
             {
+                // 48. InverseLerp은 0과 1사이의 값을 리턴한다.
+                // 예를 들어 노이즈맵값이 최소 노드 높이와 같고 최대값과 같으면 0을 반환하고 노드 높이는 1을 반환한다.
                 noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]); 
             }
         }
         // 13. noiseMap을 반환해준다.
+        // 47. 원하는 noiseMap을 반환하기 위해 정상화하고 값은 0에서 1의 범위로 돌아간다, 그러기위해서 noiseMap에서 최저값과 최고값을 추적해야된다.
          return noiseMap;
     }
 }
