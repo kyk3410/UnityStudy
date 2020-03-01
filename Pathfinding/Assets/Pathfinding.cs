2230eb954f6b7f7d66e5538519f3a13cfcc9376b
YangKyuDeskTop <kyk8229@naver.com>
2020-03-01 23:26:06 +0900

    3-1 commit

---
+++ Pathfinding/Assets/Pathfinding.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    Grid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();   
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode= grid.NodeFromWorldPoint(targetPos);
+    }
+}
