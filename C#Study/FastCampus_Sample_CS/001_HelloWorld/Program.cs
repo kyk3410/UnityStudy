8196bbee848e2be27d844ab0c8036718de07b00d
YangKyuDeskTop <kyk8229@naver.com>
2020-06-02 23:34:38 +0900

    c# 공부

---
+++ C#Study/FastCampus_Sample_CS/001_HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: _001_HelloWorld
+ */
+
+namespace _001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld"); // System.Console
+        }
+    }
+}
---
+++ C#Study/FastCampus_Sample_CS/002_HelloWorld2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                Console.WriteLine("Hello World: " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello World: args.Length = 0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
