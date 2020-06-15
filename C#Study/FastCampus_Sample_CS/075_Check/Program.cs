d0f07c37bf8806b1049fad4be87ca6555c9ff81f
YangKyuDeskTop <kyk8229@naver.com>
2020-06-15 22:44:20 +0900

    c# commit

---
+++ C#Study/FastCampus_Sample_CS/075_Check/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _075_Check
+{
+    class Program
+    {
+        static void InputID(int[] ID, int index)
+        {
+
+        }
+
+        static void InputKor(int[] kor, int index)
+        {
+
+        }
+        static void InputMath(int[] math, int index)
+        {
+
+        }
+        static void InputEng(int[] eng, int index)
+        {
+
+        }
+        static void OrintId(int max, int[] ID)
+        {
+
+        }
+
+        static int CheckID(int id, int max, int[] ID)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] InputID = new int[3];
+            Console.Write("학생 ID를 입력하세요? ");
+            InputID = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ C#Study/FastCampus_Sample_CS/CheckPoint03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// CheckPoint03 - 배열을 활용하기
+namespace CheckPoint03
+{
+    class Program
+    {
+        const int MAP_X = 7;
+        const int MAP_Y = 22;
+
+        static void UpdateView(char[] _tile, int[,] _map)// 배열을 파라미터로 넘긴다
+        {
+            for (int i = 0; i < MAP_X; i++)
+            {
+                for (int j = 0; j < MAP_Y; j++)
+                {
+                    int tileIndex = _map[i, j];
+                    Console.Write(_tile[tileIndex]);
+
+                    if (j == MAP_Y - 1)
+                        Console.WriteLine();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            //              0    1    2    3    4    5    6    7
+            char[] tile = {' ', '-', '|', '1', '2', '3', '4', '5'};
+
+            int[,] map = new int[MAP_X, MAP_Y]
+            {
+                //0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20  21
+                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}, //0
+                 {3, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, //1
+                 {4, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, //2
+                 {5, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, //3
+                 {6, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, //4
+                 {7, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}, //5
+                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}, //6
+            };
+
+            UpdateView(tile, map);
+        }
+    }
+}
