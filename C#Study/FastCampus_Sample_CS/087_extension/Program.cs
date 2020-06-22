473d9760753c892a56b440234d6f779a45d69bd4
YangKyuDeskTop <kyk8229@naver.com>
2020-06-22 21:59:57 +0900

    c#

---
+++ C#Study/FastCampus_Sample_CS/087_extension/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 확장 함수
+namespace _087_extension
+{
+    class AA
+    {
+        public void View(string str)
+        {
+            Console.WriteLine("PrintAA {0}", str);
+        }
+    }
+
+    static class Util
+    {
+        public static void Print(this AA aa, string str)
+        {
+            aa.View(str);
+        }
+
+        public static void Sum(this int a)
+        {
+            Console.WriteLine("{0} + {1} = {0}", a, a, a + a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+
+            Util.Print(aa, "Hello");
+            aa.Print("Hello"); //이게 가능
+
+            Util.Sum(10);
+            10.Sum(); // 이게 가능
+        }
+    }
+}
